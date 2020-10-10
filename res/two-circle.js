d7e2d0e272b9e5b6111a3935383a82dfec05fb6d
Brendan Horng <brendan.horng@ludusamo.com>
2020-10-10 15:19:59 -0400

    feat(art): add two circle

---
+++ res/two-circle.js
@@ -0,0 +1,34 @@
+export const art = p => {
+  const WIDTH = 300
+  const HEIGHT = 300
+
+  const MIN_DIAMETER = 15
+  const MAX_DIAMETER = 100
+
+  const SATURATION = 75
+  const VALUE = 66
+
+  p.setup = function() {
+    p.createCanvas(WIDTH, HEIGHT);
+  }
+
+  p.draw = function() {
+    p.colorMode(p.HSB)
+    const circleHue = p.abs(p.sin(p.millis() / 5223) * 255)
+    const backgroundHue = (circleHue + 120) % 360
+    const circleHue2 = (backgroundHue + 120) % 360
+    p.background(backgroundHue, SATURATION, VALUE)
+
+    const diameter = MIN_DIAMETER + ((MAX_DIAMETER - MIN_DIAMETER) * p.abs(p.sin(p.millis() / 2134)))
+    const maxBound = (WIDTH / 2) - (diameter / 2)
+
+    p.fill(circleHue, SATURATION, VALUE)
+    p.circle((WIDTH / 2) + maxBound * p.sin(p.millis() / 1500 - p.HALF_PI),
+             (HEIGHT / 2) + maxBound * p.sin(p.millis() / 750 - p.HALF_PI),
+             diameter)
+    p.fill(circleHue2, SATURATION, VALUE)
+    p.circle((WIDTH / 2) + maxBound * p.sin(p.millis() / 1500 + p.HALF_PI),
+             (HEIGHT / 2) + maxBound * p.sin(p.millis() / 750 + p.HALF_PI),
+             diameter)
+  }
+}
